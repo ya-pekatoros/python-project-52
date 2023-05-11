d12791040bd3e5cee0ff6e0b2577c2244451d173
Yaroslav Pekatoros <y.pekatoros@gmail.com>
2023-05-11 14:16:17 +0300

    upd settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import dj_database_url
 from django.utils.translation import gettext_lazy as _
+from django.urls import reverse_lazy
 from dotenv import load_dotenv
 import os
 from pathlib import Path
@@ -21,7 +22,9 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+LOGIN_URL = reverse_lazy('login')
+LOGIN_REDIRECT_URL = reverse_lazy('index')
+LOGOUT_REDIRECT_URL = reverse_lazy('index')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
